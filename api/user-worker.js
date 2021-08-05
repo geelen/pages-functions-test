52eb5f6cdfdd62ab77ddfaeb1cdec7212cbc8398
Glen Maddern <glenmaddern@gmail.com>
2021-08-05 21:18:16 +0100

    Update user-worker.js

--- api/user-worker.js
+++ api/user-worker.js
@@ -10,23 +10,9 @@ export default {
     const input = {
       headers, url, method, body: await request.text()
     }
-    console.log(input)
-    
-    const assets = env.ASSETS || env.ASSETS__DO_NOT_USE
-    let response = null
-    
-    if (assets) {
-      const res = await ().fetch(request)
-      const body = await res.text()
-      response = {
-        headers: [...res.headers.entries()],
-        body
-      }
-    }
     
     const payload = {
       request: input,
-      response
     }
     
     return new Response(JSON.stringify(payload), {
