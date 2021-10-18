f0b8b87b302dac38bdab2f848d155d1866180f35
Glen Maddern <glenmaddern@gmail.com>
2021-10-18 13:23:47 +0100

    Create worker.mjs

---
+++ worker.mjs
@@ -0,0 +1,25 @@
+export default {
+  async fetch(request, env, ctx) {
+    const { url, method } = request
+    const headers = {}
+
+    for (const [k, v] of request.headers.entries()) {
+      headers[k] = v
+    }
+
+    const payload = {
+      headers,
+      url,
+      method,
+      body: await request.text(),
+      env,
+      ctx
+    }
+    console.log(payload)
+    return new Response(JSON.stringify(payload), {
+      headers: {
+        'content-type': 'application/json',
+      },
+    })
+  },
+}
